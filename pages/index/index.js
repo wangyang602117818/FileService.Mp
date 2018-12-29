e1ae64853cbc64d1031f90f656f86ff65cfa2a78
wangyang <18518317472@163.com>
2018-12-29 16:24:48 +0800

    s

--- pages/index/index.js
+++ pages/index/index.js
@@ -12,34 +12,88 @@ Page({
     orderField: "CreateTime",
     orderFieldType: "desc",
     count: 0,
-    showLoading:false,
+    showLoading: false,
     showOrder: false,
     showAdd: false,
+    showBottomFun: false,
     end: false,
+    selectedIds: [],
     result: []
   },
   con_tap(e) {
-    if (this.data.showOrder) this.setData({ showOrder: false });
-    if (this.data.showAdd) this.setData({ showAdd: false });
+    if (this.data.showOrder) this.setData({
+      showOrder: false
+    });
+    if (this.data.showAdd) this.setData({
+      showAdd: false
+    });
   },
-  addimage(e){
-
+  funBack(e) {
+    for (var i = 0; i < this.data.result.length; i++) {
+      this.data.result[i].selected = false;
+    }
+    this.setData({
+      result: this.data.result,
+      showBottomFun: false,
+      selectedIds: []
+    }, function() {
+      wx.showTabBar({
+        animation: true
+      });
+    })
+  },
+  selectItem(e) {
+    var id = e.target.id;
+    for (var i = 0; i < this.data.result.length; i++) {
+      if (this.data.result[i]._id.$oid == id) {
+        this.data.result[i].selected = !this.data.result[i].selected;
+      }
+    }
+    this.setData({
+      result: this.data.result
+    });
+    var selectedIds = [];
+    for (var i = 0; i < this.data.result.length; i++) {
+      if (this.data.result[i].selected) selectedIds.push(this.data.result[i]._id.$oid);
+    }
+    if (selectedIds.length > 0) {
+      wx.hideTabBar({
+        animation: true,
+        success: function() {
+          this.setData({
+            showBottomFun: true,
+            selectedIds: selectedIds
+          });
+        }.bind(this)
+      });
+    } else {
+      this.setData({
+        showBottomFun: false,
+        selectedIds: selectedIds
+      }, function() {
+        wx.showTabBar({
+          animation: true
+        });
+      });
+    }
   },
   showOrder(e) {
     this.setData({
       showOrder: !this.data.showOrder,
-      showAdd:false
+      showAdd: false
     })
   },
-  showAdd(e){
+  showAdd(e) {
     this.setData({
       showAdd: !this.data.showAdd,
-      showOrder:false
+      showOrder: false
     })
   },
   changOrder(e) {
     this.data.pageIndex = 1;
-    this.setData({end:false});
+    this.setData({
+      end: false
+    });
     var field = e.target.id;
     if (field) {
       this.setData({
@@ -49,25 +103,28 @@ Page({
     }
     this.getData(false, null, true);
   },
-  onLoad: function () {
+  onLoad: function() {
+    // wx.hideTabBar({});
     this.data.pageIndex = 1;
     this.getData(false, null, true);
   },
-  search: function (e) {
+  search: function(e) {
     this.data.pageIndex = 1;
-    this.setData({ end: false });
+    this.setData({
+      end: false
+    });
     var value = e.detail.value;
     this.setData({
       filter: value
-    }, function () {
+    }, function() {
       this.getData(false, null, true);
     }.bind(this));
   },
-  getData: function (append, callback, loading) {
+  getData: function(append, callback, loading) {
     var url = app.baseUrl + "files/getfiles?" + "pageIndex=" + this.data.pageIndex + "&pageSize=" + this.data.pageSize + "&filter=" + this.data.filter;
     if (this.data.orderField) url = url + "&orderField=" + this.data.orderField;
     if (this.data.orderFieldType) url = url + "&orderFieldType=" + this.data.orderFieldType;
-    app.get(url, {}, function (data) {
+    app.get(url, {}, function(data) {
       if (data.code == 0) {
         //util.setKeyWord(data, this.data.filter);
         if (this.data.pageIndex * this.data.pageSize > data.count) {
@@ -93,15 +150,19 @@ Page({
       if (callback) callback();
     }.bind(this), loading)
   },
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
     this.data.pageIndex = 1;
-    this.setData({ end: false });
-    this.getData(false, function () {
+    this.setData({
+      end: false
+    });
+    this.getData(false, function() {
       wx.stopPullDownRefresh();
     }, true);
   },
-  onReachBottom: function () {
-    this.setData({ showLoading: true});
+  onReachBottom: function() {
+    this.setData({
+      showLoading: true
+    });
     if (!this.data.end) {
       this.data.pageIndex = this.data.pageIndex + 1;
       this.getData(true);
