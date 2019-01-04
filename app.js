ddc20432a8851df154eed3c65cac0df07f21e629
wangyang <18518317472@163.com>
2019-01-04 14:20:48 +0800

    0

--- app.js
+++ app.js
@@ -6,19 +6,26 @@ App({
     wx.login({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
-        this.post(this.baseUrl + "home/weChatLogin", {
-          authCode: this.authCode,
-          apiType: this.apiType,
-          code: res.code
-        }, function(result) {
-          if (result.code == 0) {
-            this.token = result.result;
-            this.getExtensions();
-            wx.switchTab({
-              url: '/pages/index/index'
-            })
-          }
-        }.bind(this), true);
+        if (this.token.length > 0) {
+          this.getExtensions();
+          wx.switchTab({
+            url: '/pages/index/index'
+          })
+        } else {
+          this.post(this.baseUrl + "home/weChatLogin", {
+            authCode: this.authCode,
+            apiType: this.apiType,
+            code: res.code
+          }, function(result) {
+            if (result.code == 0) {
+              this.token = result.result;
+              this.getExtensions();
+              wx.switchTab({
+                url: '/pages/index/index'
+              })
+            }
+          }.bind(this), true);
+        }
       }
     })
   },
@@ -93,10 +100,10 @@ App({
       }
     })
   },
-  baseUrl: "http://192.168.1.103:5000/api/",
+  baseUrl: "http://127.0.0.1:5000/api/",
   authCode: "1936aef6d2ba",
   apiType: "none",
-  token: "",
+  token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoid2FuZyIsIkFwcE5hbWUiOiJNaW5pIFByb2dyYW0iLCJBcGlUeXBlIjoibm9uZSIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6ImFkbWluIiwiZXhwIjoxNTQ2NDQ0Nzk5LCJpc3MiOiJodHRwOi8vMTI3LjAuMC4xLyIsImF1ZCI6Imh0dHA6Ly8xMjcuMC4wLjEvIn0.Nt4tdO6yuCoxtvrbrB2hlRV22k6-sy0sOCLi_1TKkF0",
   extensions: [],
   funs: require('./utils/util.js'),
   globalData: {
