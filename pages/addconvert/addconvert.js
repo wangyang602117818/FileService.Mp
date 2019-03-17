232f3a4d0fad1cfce502b2b4608fbb13eb5ce2a6
wangyang <60211781@qq.com>
2019-03-17 18:53:00 +0800

    s

--- pages/addconvert/addconvert.js
+++ pages/addconvert/addconvert.js
@@ -1,9 +1,111 @@
 const app = getApp()
+const util = require("../../utils/util.js")
 Page({
   data: {
-
+    flag: "",
+    format: 0,
+    model: 0,
+    x: 0,
+    y: 0,
+    width: 0,
+    height: 0,
+    imageQuality: 0,
+    formatItems: ['默认', 'Jpeg', 'Png', "Gif", "Bmp"],
+    modelItems: ['缩放', '剪切', '按宽度', '按高度'],
+    imageQualityItems: ['高', '中', '低'],
+    maxWidth: 0,
+    maxHeight: 0
   },
-  onLoad: function () {
+  flagChange: function(e) {
+    this.setData({
+      flag: e.detail.value
+    });
+  },
+  sliderChangeTop: function(e) {
+    this.setData({
+      y: e.detail.value
+    });
+  },
+  sliderChangeLeft: function(e) {
+    this.setData({
+      x: e.detail.value
+    });
+  },
+  sliderChangeWidth: function(e) {
+    this.setData({
+      width: e.detail.value
+    });
+  },
+  sliderChangeHeight: function(e) {
+    this.setData({
+      height: e.detail.value
+    });
+  },
+  showOutputFormat: function(e) {
+    wx.showActionSheet({
+      itemList: this.data.formatItems,
+      success: function(res) {
+        this.setData({
+          format: res.tapIndex
+        });
+      }.bind(this)
+    })
+  },
+  showConvertModel: function(e) {
+    wx.showActionSheet({
+      itemList: this.data.modelItems,
+      success: function(res) {
+        this.setData({
+          model: res.tapIndex
+        });
+      }.bind(this)
+    })
+  },
+  showQualityModel: function(e) {
+    wx.showActionSheet({
+      itemList: this.data.imageQualityItems,
+      success: function(res) {
+        this.setData({
+          imageQuality: res.tapIndex
+        });
+      }.bind(this)
+    })
+  },
+  ok: function(e) {
+    if (util.trim(this.data.flag) == "") {
+      return util.toast("标识不能为空")
+    }
+    let pages = getCurrentPages();
+    let prevPage = pages[pages.length - 2];
+    prevPage.data.converts.push({
+      flag: this.data.flag,
+      format: this.data.format,
+      model: this.data.model,
+      x: this.data.x,
+      y: this.data.y,
+      width: this.data.width,
+      height: this.data.height,
+      imageQuality: this.data.imageQuality
+    });
+    prevPage.setData({
+      converts: prevPage.data.converts
+    });
+    wx.navigateBack({
+      delta: 1
+    })
+  },
+  onLoad: function(options) {
+    options.index >= 0 ? wx.setNavigationBarTitle({
+      title: '修改转换'
+    }) : wx.setNavigationBarTitle({
+      title: '添加转换'
+    })
+
+    this.setData({
+      maxWidth: options.maxWidth || 0,
+      maxHeight: options.maxHeight || 0,
+
+    })
 
   }
 })
--- pages/addimage/addimage.js
+++ pages/addimage/addimage.js
@@ -2,19 +2,29 @@ const app = getApp()
 Page({
   data: {
     imageUrls: [],
+    converts: [],
+    modelItems: ['缩放', '剪切', '按宽度', '按高度'],
     longPressIndex: 0
   },
+  maxWidth: 0,
+  maxHeight: 0,
   previewFile: function(e) {
+    if (this.data.longPressIndex > 0) {
+      this.setData({
+        longPressIndex: 0
+      });
+      return;
+    }
     var url = e.currentTarget.dataset.url;
     wx.previewImage({
-      current:url,
+      current: url,
       urls: this.data.imageUrls
     })
   },
   longPress(e) {
     var url = e.currentTarget.dataset.url;
     for (var i = 0; i < this.data.imageUrls.length; i++) {
-      if (this.data.imageUrls[i] == url) {
+      if (this.data.imageUrls[i].toString() == url.toString()) {
         var longPressIndex = i + 1;
         this.setData({
           longPressIndex: longPressIndex
@@ -23,20 +33,56 @@ Page({
       }
     }
   },
-  removeItem:function(e){
+  removeItem: function(e) {
     var id = e.currentTarget.dataset.id;
-    this.data.imageUrls.splice(id,1);
-    this.setData({ imageUrls: this.data.imageUrls, longPressIndex:0})
+    this.data.imageUrls.splice(id, 1);
+    this.setData({
+      imageUrls: this.data.imageUrls,
+      longPressIndex: 0
+    }, function() {
+      this.checkImageMax(this.data.imageUrls);
+    }.bind(this))
   },
   chooseImage: function() {
     var that = this;
     wx.chooseImage({
       success: function(res) {
+        res.tempFilePaths.forEach(function(value, index) {
+          that.data.imageUrls.push(value);
+        });
         that.setData({
-          imageUrls: res.tempFilePaths
-        })
+          imageUrls: that.data.imageUrls
+        });
+        that.checkImageMax(that.data.imageUrls);
       },
     })
+  },
+  addConvert() {
+    wx.navigateTo({
+      url: "/pages/addconvert/addconvert?maxWidth=" + this.maxWidth + "&maxHeight=" + this.maxHeight
+    })
+  },
+  updateConvert(e) {
+    var index = e.currentTarget.dataset.key;
+    var convert = this.data.converts[index];
+    wx.navigateTo({
+      url: "/pages/addconvert/addconvert?index=" + index + "&flag=" + convert.flag + "&format=" + convert.format + "&model=" + convert.model + "&x=" + convert.x + "&y=" + convert.y + "&width=" + convert.width + "&height=" + convert.height + "&imageQuality=" + convert.imageQuality
+    })
+  },
+  checkImageMax(urls) {
+    var that = this;
+    that.maxWidth = 0;
+    that.maxHeight = 0;
+    for (var i = 0; i < urls.length; i++) {
+      var url = urls[i];
+      wx.getImageInfo({
+        src: url,
+        success: function(data) {
+          if (data.width > that.maxWidth) that.maxWidth = data.width;
+          if (data.height > that.maxHeight) that.maxHeight = data.height;
+        }
+      })
+    }
   },
   onLoad: function() {
 
